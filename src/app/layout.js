bc42dae25e772bafb0b5cf871c6ff8e76589ed80
gptjd0204 <gptjd0204@naver.com>
2026-06-02 18:07:20 +0900

    feat: 전역 color 변수 및 폰트 설정

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import QueryProvider from "@/providers/QueryProvider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const notoSansKr = Noto_Sans_KR({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
 });
 
@@ -19,10 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="ko" className={`${notoSansKr.variable} h-full antialiased`}>
       <body className="min-h-full">
         <QueryProvider>{children}</QueryProvider>
       </body>
---
+++ src/fonts/index.js
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+export const brBold = localFont({
+  src: "./BR_B.otf",
+  display: "swap",
+});
+
+export const brRegular = localFont({
+  src: "./BR_R.otf",
+  display: "swap",
+});

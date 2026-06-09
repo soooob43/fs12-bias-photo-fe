e8dd3f2cd493becc56c3d2a2a23b240f89be4931
gptjd0204 <gptjd0204@naver.com>
2026-06-09 17:04:07 +0900

    chore: defaultFetch 캐시 설정 변경

--- src/api/core/fetchClient.js
+++ src/api/core/fetchClient.js
@@ -14,7 +14,7 @@ export const defaultFetch = async (url, options = {}) => {
     headers: {
       'Content-Type': 'application/json',
     },
-    cache: 'force-cache',
+    cache: 'no-store',
   };
 
   const mergedOptions = {

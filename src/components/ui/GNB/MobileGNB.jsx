733edd107d015187cff6532d9adb48eb08da21af
gptjd0204 <gptjd0204@naver.com>
2026-06-12 18:48:25 +0900

    feat: 모바일용 공통 GNB 컴포넌트 구현

---
+++ src/components/ui/GNB/MobileGNB.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import LeftArrowIcon from '@/components/icons/LeftArrowIcon';
+import React from 'react';
+import { brBold } from '@/fonts';
+import { useRouter } from 'next/navigation';
+
+const MobileGNB = ({ title }) => {
+  const router = useRouter();
+  return (
+    <header className="relative flex items-center justify-center py-[1.25rem] mx-auto bg-(--black-black) md:hidden">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="cursor-pointer"
+      >
+        <LeftArrowIcon className="absolute top-1/2 -translate-y-1/2 left-1" />
+      </button>
+      <h1 className={`${brBold.className} text-[1.25rem] text-(--white-white)`}>
+        {title}
+      </h1>
+    </header>
+  );
+};
+
+export default MobileGNB;

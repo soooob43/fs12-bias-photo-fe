270dd27252a7d07ac13c315dee3c5049501f3483
gptjd0204 <gptjd0204@naver.com>
2026-06-12 11:51:49 +0900

    feat: QueryProvider 로직 수정

--- src/providers/QueryProvider.jsx
+++ src/providers/QueryProvider.jsx
@@ -1,22 +1,39 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 10,
+        refetchOnWindowFocus: false,
+        refetchOnMount: true,
+      },
+    },
+  });
+}
+
+let browserQueryClient = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    return makeQueryClient();
+  } else {
+    // 브라우저에서는 이미 만들어진 쿼리 클라이언트를 반환
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 export default function QueryProvider({ children }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60,
-            refetchOnWindowFocus: false,
-          },
-        },
-      }),
-  );
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
   );
 }

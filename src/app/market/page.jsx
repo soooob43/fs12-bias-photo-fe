7c7518f797d116bc7853cfee0a678d735555d27b
gptjd0204 <gptjd0204@naver.com>
2026-06-16 09:57:16 +0900

    feat: 비로그인 상태에서 상호작용시 로그인 필요 모달창 표시 구현

--- src/app/market/page.jsx
+++ src/app/market/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ClientPage from './page.client';
+import MarketClientPage from './page.client';
 
 const MarketPlacePage = () => {
   return (
     <div className="w-full max-w-[1480px] mx-auto px-[15px] pb-[90px] md:pb-0">
-      <ClientPage />
+      <MarketClientPage />
     </div>
   );
 };

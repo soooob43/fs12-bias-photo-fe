e59108caf5e75bfc1a333299ef5aa6c00cc19eb2
gptjd0204 <gptjd0204@naver.com>
2026-06-16 12:15:09 +0900

    feat: 마켓플레이스 공통 헤더 추가

--- src/app/market/page.jsx
+++ src/app/market/page.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import MarketClientPage from './page.client';
+import MarketPlaceHeader from './(components)/MarketPlaceHeader';
 
 const MarketPlacePage = () => {
   return (
-    <div className="w-full max-w-[1480px] mx-auto px-[15px] pb-[90px] md:pb-0">
-      <MarketClientPage />
-    </div>
+    <>
+      <MarketPlaceHeader />
+      <div className="w-full max-w-[1480px] mx-auto px-[15px] pb-[90px] md:pb-0">
+        <MarketClientPage />
+      </div>
+    </>
   );
 };
 

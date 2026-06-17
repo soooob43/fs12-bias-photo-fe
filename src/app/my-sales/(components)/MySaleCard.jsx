0d2e598a988974ac5f778a2e768d573370b68f69
Han Hui Na <hanhina05@gmail.com>
2026-06-17 10:39:04 +0900

    refactor: MySaleCard 상수 분리

--- src/app/my-sales/(components)/MySaleCard.jsx
+++ src/app/my-sales/(components)/MySaleCard.jsx
@@ -3,46 +3,13 @@ import React from 'react';
 import imgTest from '@/assets/images/img_photo_card_test.svg';
 import imgLogo from '@/assets/images/img_logo.svg';
 import icSoldOut from '@/assets/icons/ic_sold_out.svg';
-
-// 등급에 따른 색깔 설정 객체
-const GRADE_COLORS = {
-  COMMON: 'text-(--main-main)',
-  RARE: 'text-(--blue-blue)',
-  SUPER_RARE: 'text-(--purple-purple)',
-  LEGENDARY: 'text-(--pink-pink)',
-};
-
-// 등급 이름 매핑 객체
-const GRADE_MAP = {
-  COMMON: 'COMMON',
-  RARE: 'RARE',
-  SUPER_RARE: 'SUPER RARE',
-  LEGENDARY: 'LEGENDARY',
-};
-
-// 장르 이름 매핑 객체
-const GENRE_MAP = {
-  ALBUM: '앨범',
-  BENEFIT: '특전',
-  FAN_SIGN: '팬싸',
-  SEASON_GREETING: '시즌그리팅',
-  FAN_MEETING: '팬미팅',
-  CONCERT: '콘서트',
-  MD: 'MD',
-  COLLAB: '콜라보',
-  FAN_CLUB: '팬클럽',
-  ETC: '기타',
-};
-
-const STATUS_MAP = {
-  ON_SALE: '판매 중',
-  ON_EXCHANGE: '교환 제시 대기 중',
-};
-
-const STATUS_STYLE = {
-  ON_SALE: 'text-(--white-white)',
-  ON_EXCHANGE: 'text-(--main-main)',
-};
+import {
+  GRADE_COLORS,
+  GRADE_MAP,
+  GENRE_MAP,
+  STATUS_MAP,
+  STATUS_STYLE,
+} from '@/constants/mySaleCard';
 
 const MySaleCard = ({
   type = 'default',

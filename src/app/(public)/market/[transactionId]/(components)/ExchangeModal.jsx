c518adf538fb3bf743b5532289e8f249d4f41420
신영미 <sin-yeongmi@MacBook-Air.local>
2026-06-18 12:04:56 +0900

    [feat] 교환 모달 설명 255자 제한, 설명 없으면 버튼 비활성화 및 입력 요청 메시지 출력

--- src/app/(public)/market/[transactionId]/(components)/ExchangeModal.jsx
+++ src/app/(public)/market/[transactionId]/(components)/ExchangeModal.jsx
@@ -12,8 +12,10 @@ export default function ExchangeModal({
   cardInfo,
   onClose,
 }) {
-  const [exdescription, setExdescription] = useState(''); //입력받은 교환 요청 설명 기입
+  const [exdescription, setExdescription] = useState(''); //입력받은 교환 제시 설명 기입
   const queryClient = useQueryClient();
+
+  const isDescriptionEmpty = !exdescription || !exdescription.trim(); // 교환 제시가 공란인지 확인
 
   if (!cardInfo) {
     alert('카드 정보가 조회되지 않습니다!');
@@ -21,8 +23,7 @@ export default function ExchangeModal({
   }
 
   const handleExchange = async () => {
-    if (!exdescription || !exdescription.trim()) {
-      alert('교환 제시 내용을 입력해주세요.');
+    if (isDescriptionEmpty) {
       return;
     }
 
@@ -108,13 +109,23 @@ export default function ExchangeModal({
           <p className="mb-[0.62rem] text-[#FFF] font-['Noto_Sans_KR'] text-[1.25rem] font-bold">
             교환 제시 내용
           </p>
-          <textarea
-            type="text"
-            value={exdescription}
-            onChange={(e) => setExdescription(e.target.value)}
-            placeholder="내용을 입력해주세요."
-            className="w-[34rem] h-[7.9rem] px-[1.125rem] py-[1.25rem] rounded-[0.125rem] text-[#FFF] border border-[#DDD] font-['Noto_Sans_KR'] text-[1rem] focus:outline-none focus:border-[#EFFF04]"
-          />
+          <div className="relative pb-[2rem]">
+            <textarea
+              type="text"
+              value={exdescription}
+              onChange={(e) => setExdescription(e.target.value)}
+              maxLength={255} //최대 255자 수용
+              placeholder="내용을 입력해주세요."
+              className="w-[34rem] h-[7.9rem] px-[1.125rem] py-[1.25rem] rounded-[0.125rem] text-[#FFF] border border-[#DDD] font-['Noto_Sans_KR'] text-[1rem] focus:outline-none focus:border-[#EFFF04]"
+            />
+
+            {isDescriptionEmpty && (
+              <p className="absolute bottom-0 left-0 text-[#FF4D4D] font-['Noto_Sans_KR'] text-[0.875rem]">
+                교환 제시 내용을 입력해주세요!
+              </p>
+            )}
+          </div>
+
           <div className="flex gap-[1.25rem]">
             <button
               onClick={onClose}
@@ -125,7 +136,8 @@ export default function ExchangeModal({
 
             <button
               onClick={handleExchange}
-              className="w-full h-[60px] flex justify-center items-center mt-[3.75rem] rounded-[0.125rem] bg-[#EFFF04] cursor-pointer text-[#0F0F0F] font-['Noto_Sans_KR'] text-[1.125rem] font-bold"
+              disabled={isDescriptionEmpty}
+              className="w-full h-[60px] flex justify-center items-center mt-[3.75rem] rounded-[0.125rem] bg-[#EFFF04] cursor-pointer disabled:bg-[#949494] disabled:cursor-not-allowed text-[#0F0F0F] font-['Noto_Sans_KR'] text-[1.125rem] font-bold"
             >
               교환하기
             </button>
